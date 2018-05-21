5d4d08071c23950528f89bb398f6962edebe9867
Roger Bassons Renart <rogerbassons@gmail.com>
2018-05-21 20:32:13 +0200

    fix hour overflow

--- changeTimezone.py
+++ changeTimezone.py
@@ -1,4 +1,6 @@
 from sys import argv
+import dateutil.parser
+from datetime import timedelta
 
 shift = int(argv[2])
 output = open(argv[1].split(".")[0] + "-adjusted.gpx", "w")
@@ -7,15 +9,23 @@ with open(argv[1]) as f:
     for line in f:
         if "<time>" in line:
             s = line.split("T")
-            time = s[1].split(":")
+            time = s[1].split("Z")[0].split(":")
+            date = s[0].replace('<time>', '').strip()
+            indentation = s[0].split("<")[0]
             
-            new = int(time[0]) + shift
+            newHour = int(time[0]) + shift
+            if newHour < 0:
+                date  = (dateutil.parser.parse(date) - timedelta(1)).isoformat().split("T")[0]
+                newHour = 24 + newHour
+            elif newHour >= 24:
+                date = (dateutil.parser.parse(date) + timedelta(1)).isoformat().split("T")[0]
+                newHour = 0 + newHour - 24
 
-            newStr = str(new)
-            if new < 10:
-                newStr = "0" + newStr
+            newHourStr = str(newHour)
+            if newHour < 10:
+                newHourStr = "0" + newHourStr
 
-            output.write(s[0] + "T" + newStr + ":" + time[1] + ":" + time[2].split("Z")[0] + "Z</time>\n")
+            output.write(indentation + "<time>" + date + "T" + newHourStr + ":" + time[1] + ":" + time[2] + "Z</time>\n")
 
         else:
             output.write(line)
